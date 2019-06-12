39c9a2b69aea177004b8ca4d85c11eb27754f8dc
Kamila <kamila.kalecinska@gmail.com>
2019-06-12 14:17:50 +0200

    Arguments are now good

--- demo.py
+++ demo.py
@@ -7,10 +7,10 @@ import os, random
 from data_preprocessing import create_data_generators
 import json
 
-# def load_constants()
-RESULTS_FOLDER = "../saved_models/20190612_1048"
-HYPERPARAMS_FILE =  RESULTS_FOLDER+ '/hyperparams.json'
-
+# arguments given in terminal: RESULTS_FOLDER, CAR_CLASS
+# default
+RESULTS_FOLDER = "../saved_models/20190610_1512"
+#20190612_1048
 if (os.getcwd() == '/home/kalkami/translearn'):
     #lhcpgpu1
     TRAIN_DIR = '/data/IntelliGate/kalkami/DATASETS/carsStanford_all/train'
@@ -23,37 +23,24 @@ else:
     TEST_DIR = '/media/kamila/System/Users/Kama/Documents/DATASETS/carsStanford_s/test'
     TRAIN_DIR_TST = '/media/kamila/System/Users/Kama/Documents/DATASETS/carsStanford_all/train'
     TEST_DIR_TST = '/media/kamila/System/Users/Kama/Documents/DATASETS/carsStanford_all/test'
+    #TEST_DIR_TST = '/media/kamila/System/Users/Kama/Documents/DATASETS/CARS_GOOGLE_IMG/downloads'
 
-with open(HYPERPARAMS_FILE, "r") as read_file:
-    data = json.load(read_file)
 
-HYPERPARAMS = data['hyperparameters'][0]
-EPOCHS = HYPERPARAMS['EPOCHS']
-FC_LAYERS = HYPERPARAMS['FC_LAYERS']
-EPOCHS = HYPERPARAMS['DROPOUT']
-WEIGHTS = HYPERPARAMS['WEIGHTS']
-TRAIN_LAYERS = HYPERPARAMS['TRAIN_LAYERS']
-BATCHSIZE = HYPERPARAMS['BATCHSIZE']
-DROPOUT = HYPERPARAMS['DROPOUT']
-NEW_WEIGHTS = HYPERPARAMS['NEW_WEIGHTS'] 
-INPUT_SHAPE = (299,299,3) # try to take it from model
-#input_shape = model.layers[0].output_shape[1:3]
-
-def load_image(img_path, input_shape, show=True):
-
+def load_image(img_path, input_shape, show=False):
+    img_org = image.load_img(img_path) 
     img = image.load_img(img_path, target_size=input_shape)
     img_tensor = image.img_to_array(img)                    # (height, width, channels)
     img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
     img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
 
     if show:
-        plt.imshow(img_tensor[0])                           
+        plt.imshow(img_org)                           
         plt.axis('off')
         plt.show()
 
     return img_tensor
 
-def decode_predictions(preds, class_names, top=3):
+def decode_predictions(preds, class_names, top=5):
     results = []
     for pred in preds:
         top_indices = pred.argsort()[-top:][::-1]
@@ -62,40 +49,64 @@ def decode_predictions(preds, class_names, top=3):
         results.append(result)
     return results
 
-def predict(img_path, model, input_shape, class_names):
+def predict(img_path, model, input_shape, class_names, correct_class):
     img_array = load_image(img_path, input_shape)
     preds = model.predict(img_array)
-    print(decode_predictions(preds, class_names))
+    predictions = decode_predictions(preds, class_names)
+    top1_pred = predictions[0][0]
+    print(predictions)
+    img_org = image.load_img(img_path)
+    fig, axs = plt.subplots(1,2)
+    axs[0].set_title(correct_class)
+    axs[0].imshow(img_org)
+    #axs[0].axis('off')
+
+    axs[1].set_title(str(top1_pred))
+    axs[1].imshow(img_array[0])
+    plt.show()
     # Decode the output of the VGG16 model.
     #pred_decoded = decode_predictions(pred)[0]
     ## Print the predictions.
     #for code, name, score in pred_decoded:
         #print("{0:>6.2%} : {1}".format(score, name))
 
-io = True
-#if __name__ == "__main__":
-if io:
-    generator_train, generator_test = create_data_generators(INPUT_SHAPE, BATCHSIZE, 
-                                                                TRAIN_DIR, TEST_DIR, 
-                                                                save_augumented=None, 
-                                                                plot_imgs = False)
-    # randomly select an image from defined class
-    car_class = 'Audi A5 Coupe 2012'                                                           
-    class_names = list(generator_train.class_indices.keys())        
-    test_dir = TEST_DIR_TST + '/' + car_class
-    test_img = test_dir + '/' + random.choice(os.listdir(test_dir))
 
+def perform_pred(car_class, results_folder=RESULTS_FOLDER, test_dir=TEST_DIR_TST, img_pth=None):
+    HYPERPARAMS_FILE =  results_folder+ '/hyperparams.json'
+
+    with open(HYPERPARAMS_FILE, "r") as read_file:
+        data = json.load(read_file)
+
+    HYPERPARAMS = data['hyperparameters'][0]
+    BATCHSIZE = HYPERPARAMS['BATCHSIZE'] 
+
+    if img_pth is None:   
+        # randomly select an image from defined class     
+        test_dir = test_dir + '/' + car_class
+        test_img = test_dir + '/' + random.choice(os.listdir(test_dir))
+    else:
+        test_img = img_pth
     # load json and create model
-    json_file = open(RESULTS_FOLDER + '/model.json', 'r')
+    json_file = open(results_folder + '/model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
-    loaded_model.load_weights(RESULTS_FOLDER + "/weights.best.hdf5")
+    loaded_model.load_weights(results_folder + "/weights.best.hdf5")
     input_shape = loaded_model.layers[0].output_shape[1:3]
     print("Loaded model from disk")
-    predict(test_img, loaded_model, input_shape, class_names)
-    
+    generator_train, generator_test = create_data_generators(input_shape, BATCHSIZE, 
+                                                                TRAIN_DIR, TEST_DIR, 
+                                                                save_augumented=None, 
+                                                                plot_imgs = False) 
+    class_names = list(generator_train.class_indices.keys()) 
+    predict(test_img, loaded_model, input_shape, class_names, car_class)
+
+
+io = True
+#if __name__ == "__main__":
+if io:
+    perform_pred('Audi A5 Coupe 2012', results_folder="../saved_models/20190612_1048")
     # evaluate loaded model on test data
     # loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
     # score = loaded_model.evaluate(X, Y, verbose=0)
